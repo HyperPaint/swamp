1a731da4a3728f4856746a3d1ae93bcc6b32dc83
HyperPaint <dioman172@yandex.ru>
2022-07-15 20:09:53 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.Audio;
+
+namespace Swamp
+{
+    class Program
+    {
+        public static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<VideoMode> videoModes = new List<VideoMode>();
+            foreach (var item in VideoMode.FullscreenModes)
+            {
+                if (item.BitsPerPixel == 32)
+                {
+                    videoModes.Insert(0, item);
+                }
+            }
+            // консоль
+            foreach (var item in videoModes)
+            {
+                Console.WriteLine(item.Width + "x" + item.Height);
+            }
+
+            RenderWindow renderWindow = new RenderWindow(videoModes[7], "My Swamp");
+            renderWindow.Closed += (object sender, EventArgs e) => renderWindow.Close();
+            renderWindow.SetFramerateLimit(60);
+
+            // всего плиток
+            int tilesHorizontal = (int)renderWindow.Size.X / Tile.tileSize;
+            int tilesVertical = (int)renderWindow.Size.Y / Tile.tileSize;
+
+            const string tileSet = "tileset.png";
+
+            // вспомогательная переменная
+            Tile currentTile;
+
+            // вода
+            Tile[,] waterTiles = new Tile[tilesVertical, tilesHorizontal];
+            for (int y = 0; y < tilesVertical; y++)
+            {
+                for (int x = 0; x < tilesHorizontal; x++)
+                {
+                    currentTile = new Tile(Tile.Type.Water, tileSet, 0, 0);
+                    currentTile.setPosition(Tile.tileSize * x, Tile.tileSize * y);
+                    waterTiles[y, x] = currentTile;
+                }
+            }
+
+            // земля
+            Tile[,] earthTiles = new Tile[tilesVertical, tilesHorizontal];
+            for (int y = 0; y < tilesVertical; y++)
+            {
+                currentTile = new Tile(Tile.Type.Static, tileSet, 1, 0);
+                currentTile.setPosition(0, Tile.tileSize * y);
+                earthTiles[y, 0] = currentTile;
+            }
+            // верх и низ берег
+            for (int x = 2; x < tilesHorizontal; x++)
+            {
+                currentTile = new Tile(Tile.Type.Static, tileSet, 1, 2);
+                currentTile.setPosition(Tile.tileSize * x, 0);
+                earthTiles[0, x] = currentTile;
+                currentTile = new Tile(Tile.Type.Static, tileSet, 1, 2);
+                currentTile.setPosition(Tile.tileSize * x, Tile.tileSize * (tilesVertical - 1));
+                currentTile.setRotate(Tile.Rotate.d180);
+                earthTiles[tilesVertical - 1, x] = currentTile;
+            }
+            // левый берег
+            for (int y = 1; y < tilesVertical - 1; y++)
+            {
+                currentTile = new Tile(Tile.Type.Static, tileSet, 1, 2);
+                currentTile.setPosition(Tile.tileSize, Tile.tileSize * y);
+                currentTile.setRotate(Tile.Rotate.d270);
+                earthTiles[y, 1] = currentTile;
+            }
+            // уголки
+            currentTile = new Tile(Tile.Type.Static, tileSet, 1, 1);
+            currentTile.setPosition(Tile.tileSize, 0);
+            earthTiles[0, 1] = currentTile;
+            currentTile = new Tile(Tile.Type.Static, tileSet, 1, 1);
+            currentTile.setPosition(Tile.tileSize, Tile.tileSize * (tilesVertical - 1));
+            currentTile.setRotate(Tile.Rotate.d270);
+            earthTiles[tilesVertical - 1, 1] = currentTile;
+
+            // кувшинки и лягушки
+            Tile[,] personTiles = new Tile[tilesVertical, tilesHorizontal];
+            int x_, y_;
+            x_ = 3; y_ = 1;
+            currentTile = new Tile(Tile.Type.Static, tileSet, 2, 0);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            earthTiles[y_, x_] = currentTile;
+            currentTile = new Tile(Tile.Type.Frog, tileSet, 3);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            personTiles[y_, x_] = currentTile;
+            x_ = 4; y_ = 3;
+            currentTile = new Tile(Tile.Type.Static, tileSet, 2, 0);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            currentTile.setRotate(Tile.Rotate.d90);
+            earthTiles[y_, x_] = currentTile;
+            currentTile = new Tile(Tile.Type.Frog, tileSet, 4);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            personTiles[y_, x_] = currentTile;
+            x_ = 6; y_ = 2;
+            currentTile = new Tile(Tile.Type.Static, tileSet, 2, 0);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            currentTile.setRotate(Tile.Rotate.d270);
+            earthTiles[y_, x_] = currentTile;
+            currentTile = new Tile(Tile.Type.Frog, tileSet, 5);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            personTiles[y_, x_] = currentTile;
+            x_ = 8; y_ = 3;
+            currentTile = new Tile(Tile.Type.Static, tileSet, 2, 0);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            currentTile.setRotate(Tile.Rotate.d180);
+            earthTiles[y_, x_] = currentTile;
+            currentTile = new Tile(Tile.Type.Frog, tileSet, 6);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            personTiles[y_, x_] = currentTile;
+            // одна лягушка на берегу
+            x_ = 0; y_ = 3;
+            currentTile = new Tile(Tile.Type.Frog, tileSet, 7);
+            currentTile.setPosition(Tile.tileSize * x_, Tile.tileSize * y_);
+            personTiles[y_, x_] = currentTile;
+
+            // главный цикл
+            while (renderWindow.IsOpen)
+            {
+                renderWindow.DispatchEvents();
+
+                renderWindow.Clear();
+                for (int y = 0; y < tilesVertical; y++)
+                {
+                    for (int x = 0; x < tilesHorizontal; x++)
+                    {
+                        if (waterTiles[y,x] != null)
+                        {
+                            renderWindow.Draw(waterTiles[y, x]);
+                        }
+                        if (earthTiles[y, x] != null)
+                        {
+                            renderWindow.Draw(earthTiles[y, x]);
+                        }
+                        if (personTiles[y, x] != null)
+                        {
+                            renderWindow.Draw(personTiles[y, x]);
+                        }
+                    }
+                }
+                renderWindow.Display();
+            }
+        }
+    }
+}
---
+++ Tiles.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.Audio;
+
+namespace Swamp
+{
+    class Tile : Drawable
+    {
+        public enum Type : byte
+        {
+            Static,
+            Water,
+            Frog,
+        }
+
+        private Sprite sprite;
+        private Texture currentTexture;
+        private IntRect currentTextureRect;
+        private Vector2f currentPosition;
+        private int frame = 0;
+
+        public const int tileSize = 128;
+
+        delegate void prepareTextureRect();
+        event prepareTextureRect prepareTextureRectNotify;
+
+        public Tile(Type type, string tilesetFileName, int row, int column = 0)
+        {
+            switch (type)
+            {
+                case Type.Static:
+                    currentTexture = new Texture(tilesetFileName);
+                    currentTextureRect = new IntRect(tileSize * column, tileSize * row, tileSize, tileSize);
+                    currentPosition = new Vector2f(0f, 0f);
+                    break;
+
+                case Type.Water:
+                    prepareTextureRectNotify += prepareTextureRect_Water;
+                    currentTexture = new Texture(tilesetFileName);
+                    currentTextureRect = new IntRect(0, tileSize * row, tileSize, tileSize);
+                    currentPosition = new Vector2f(0f, 0f);
+                    break;
+
+                case Type.Frog:
+                    prepareTextureRectNotify += prepareTextureRect_Frog;
+                    currentTexture = new Texture(tilesetFileName);
+                    currentTextureRect = new IntRect(tileSize * column, tileSize * row, tileSize, tileSize);
+                    currentPosition = new Vector2f(0f, 0f);
+
+                    frame = Program.random.Next() % 120;
+                    break;
+
+                default:
+                    throw new Exception("No Tile.Type");
+            }
+            
+            sprite = new Sprite();
+            sprite.Texture = currentTexture;
+            sprite.TextureRect = currentTextureRect;
+            sprite.Position = currentPosition;
+        }
+
+        ~Tile()
+        {
+
+        }
+
+        void Drawable.Draw(RenderTarget target, RenderStates states)
+        {
+            target.Draw(sprite, states);
+            if (prepareTextureRectNotify != null) prepareTextureRectNotify();
+        }
+
+        void prepareTextureRect_Water()
+        {
+            frame++;
+            int currentTile = frame / 60;
+            if (currentTile > 3)
+            {
+                frame = 0;
+                currentTile = 0;
+            }
+            currentTextureRect.Left = currentTile * tileSize;
+            sprite.TextureRect = currentTextureRect;
+        }
+
+        void prepareTextureRect_Frog()
+        {
+            frame++;
+            int currentTile = frame / 30;
+            if (currentTile > 4)
+            {
+                frame = 0;
+                currentTile = 0;
+            }
+            currentTextureRect.Left = currentTile * tileSize;
+            sprite.TextureRect = currentTextureRect;
+        }
+
+        public void setPosition(int x, int y)
+        {
+            currentPosition.X = x;
+            currentPosition.Y = y;
+            sprite.Position = currentPosition;
+        }
+
+        public enum Rotate : int
+        {
+            d0 = 0,
+            d90 = 90,
+            d180 = 180,
+            d270 = 270,
+        }
+        public void setRotate(Rotate rotate)
+        {
+            switch (rotate)
+            {
+                case Rotate.d0:
+                    sprite.Origin = new Vector2f(tileSize, 0f);
+                    break;
+
+                case Rotate.d90:
+                    sprite.Origin = new Vector2f(0f, tileSize);
+                    break;
+
+                case Rotate.d180:
+                    sprite.Origin = new Vector2f(tileSize, tileSize);
+                    break;
+
+                case Rotate.d270:
+                    sprite.Origin = new Vector2f(tileSize, 0f);
+                    break;
+
+                default:
+                    throw new Exception("No Tile.Rotate");
+            }
+            
+            sprite.Rotation = (float)rotate;
+        }
+    }
+}
